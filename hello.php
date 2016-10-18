b07a51aa3394b1e46ae1754eeed6b8e628a9037e
1nickgreene <ngreene@redhat.com>
2016-10-18 10:52:53 -0400

    Update hello.php

--- hello.php
+++ hello.php
@@ -3,7 +3,7 @@
   <title>PHP Test</title>
  </head>
  <body>
- <?php echo '<p>Hello World</p>'; ?> 
+ <?php echo '<p>Hello Hello</p>'; ?> 
  <?php echo "<font color='red'>Royal Carrib</font>"; ?>
  </body>
 </html>
