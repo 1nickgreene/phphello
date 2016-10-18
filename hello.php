42067062a25b10a49eed824f84fcc1782e83de39
1nickgreene <ngreene@redhat.com>
2016-10-18 09:54:59 -0400

    Update hello.php

--- hello.php
+++ hello.php
@@ -3,7 +3,7 @@
   <title>PHP Test</title>
  </head>
  <body>
- <?php echo '<p>Hello World correct with Patch</p>'; ?> 
- <?php echo "<font color='red'>Missing Patch</font>"; ?>
+ <?php echo '<p>Hello World</p>'; ?> 
+ <?php echo "<font color='red'>Royal Carrib</font>"; ?>
  </body>
 </html>
