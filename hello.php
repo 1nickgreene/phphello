8691a37912313c9e689b3a62f1b470e6b199fc7e
1nickgreene <ngreene@redhat.com>
2016-12-13 10:47:19 -0500

    Update hello.php

--- hello.php
+++ hello.php
@@ -4,6 +4,6 @@
  </head>
  <body>
  <?php echo '<p>RONIN HEALTHCARE</p>'; ?> 
- <?php echo "<font color='red'>Nick and Chuck</font>"; ?>
+ <?php echo "<font color='red'>Nick</font>"; ?>
  </body>
 </html>
