085f27b231dbee8cef29622e73a2e03b4f623044
1nickgreene <ngreene@redhat.com>
2018-05-02 11:27:31 -0400

    Update hello.php

--- hello.php
+++ hello.php
@@ -3,7 +3,8 @@
   <title>PHP Test</title>
  </head>
  <body>
- <?php echo '<p>HELLO</p>'; ?> 
- <?php echo "<font color='red'>Nick</font>"; ?>
+ <h2>Welcome to My App</h2>
+ <?php echo '<p>This is a php application</p>'; ?> 
+ <?php echo "<font color='red'>Nick Greene - Red Hat, Inc.</font>"; ?>
  </body>
 </html>
